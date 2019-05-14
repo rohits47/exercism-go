ad1c7db7f6c92bca419c2c833bb36c7fb40c7eea
Rohit Sanbhadti <rohits47@gmail.com>
2019-05-13 19:00:03 -0700

    stubbed enough to compile; running the testcases fails as expected

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -4,8 +4,35 @@ package space
 // This is equivalent to 365.25 days.
 const EarthYearSeconds = 31557600
 
-// Age contains the seconds in the age and the associated planet.
-type Age struct {
-	seconds int
-	planet  string
+// EarthYearDays is the number of days that one earth year maps to on Earth
+const EarthYearDays = 365.25
+
+// MercuryYearDays is the number of days that one earth year maps to on Mercury
+const MercuryYearDays = 0.2408467
+
+// VenusYearDays is the number of days that one earth year maps to on Venus
+const VenusYearDays = 0.61519726
+
+// MarsYearDays is the number of days that one earth year maps to on Mars
+const MarsYearDays = 1.8808158
+
+// JupiterYearDays is the number of days that one earth year maps to on Jupiter
+const JupiterYearDays = 11.862615
+
+// SaturnYearDays is the number of days that one earth year maps to on Saturn
+const SaturnYearDays = 29.447498
+
+// UranusYearDays is the number of days that one earth year maps to on Uranus
+const UranusYearDays = 84.016846
+
+// NeptuneYearDays is the number of days that one earth year maps to on Neptune
+const NeptuneYearDays = 164.79132
+
+// Planet is a named planet in our solar system.
+type Planet string
+
+// Age gives the earth age in days from the given age on the given planet
+func Age(seconds float64, planet Planet) float64 {
+	var convertedAge float64
+	return convertedAge
 }
