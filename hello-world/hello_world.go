b7b920c91473f408d8ae6af9ab4d6d73099114fa
Rohit Sanbhadti <rohits47@gmail.com>
2019-05-13 17:16:57 -0700

    initial commit

---
+++ hello-world/hello_world.go
@@ -0,0 +1,6 @@
+package greeting
+
+// HelloWorld greets the brave new world of Gophers
+func HelloWorld() string {
+	return "Hello, World!"
+}
---
+++ two-fer/two_fer.go
@@ -0,0 +1,18 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package twofer should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package twofer
+
+import "fmt"
+
+// ShareWith returns a string about sharing, customized for the given
+// name string.
+func ShareWith(name string) string {
+	if len(name) == 0 {
+		name = "you"
+	}
+	retval := fmt.Sprintf("One for %s, one for me.", name)
+	return retval
+}
