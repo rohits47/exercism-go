513726214727ed49f5267f9a2d2e94b00183ad55
Rohit Sanbhadti <rohits47@gmail.com>
2019-05-17 16:23:23 -0700

    initial stubbed hamming package comments

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,9 @@
+// Package hamming contains methods to assist with calculating the Hamming
+// distance between different objects.
 package hamming
 
+// Distance calculates the Hamming distance between two DNA strands,
+// given as strings.
 func Distance(a, b string) (int, error) {
+	return 0, nil
 }
