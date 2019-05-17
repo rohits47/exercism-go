16e2a788d14542297bc423962ab7cb25f038f66e
Rohit Sanbhadti <rohits47@gmail.com>
2019-05-17 16:26:37 -0700

    initial implementation of hamming distance

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -2,8 +2,11 @@
 // distance between different objects.
 package hamming
 
+import "errors"
+
 // Distance calculates the Hamming distance between two DNA strands,
 // given as strings.
 func Distance(a, b string) (int, error) {
-	return 0, nil
+	// return 0, nil
+	return -1, errors.New("can't calculate Hamming distance")
 }
